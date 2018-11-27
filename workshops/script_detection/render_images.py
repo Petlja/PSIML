57e8e4505734f2235691676c9dbf3117d3345d47
Sasa Galic <sagalic@microsoft.com>
2018-11-27 21:40:47 +0100

    Add more samples, fix image height

--- workshops/script_detection/render_images.py
+++ workshops/script_detection/render_images.py
@@ -14,13 +14,13 @@ font_by_language = {
     "el" : "arial.ttf"
 }
 
-def render_sample(text, font_name):
+def render_sample(text, font_name, height):
     """
     Render image of the given text using the given font.
 
     Args:
         text :      Text to be rendered.
-        font_name : Fornt to be used in rendering.
+        font_name : Font to be used in rendering.
 
     Returns:
         Rendered image.
@@ -32,9 +32,14 @@ def render_sample(text, font_name):
     img = img.resize(text_size)
     img_draw = ImageDraw.Draw(img)
     img_draw.text((0, 0), text, font=fnt, fill='black')
-    return img
 
-def create_images(in_dir, out_dir):
+    # Resize rendered image ot given height.
+    scale = height / text_size[1]
+    width = int(text_size[0] * scale)
+    img_resized = img.resize((width, height), Image.ANTIALIAS)
+    return img_resized
+
+def create_images(in_dir, out_dir, height):
     """
     Based on the language specific files within given dir (files containing textual lines) renders corresponding
     images and saved them to given output directory.
@@ -63,18 +68,53 @@ def create_images(in_dir, out_dir):
         for text_line in text_lines:
             if text_line == "":
                 continue
-            img = render_sample(text_line, font_name)
+            img = render_sample(text_line, font_name, height)
             img_name = str(current_sample) + ".png"
             img_path = os.path.join(out_lang_dir, img_name)
             # Save image to output language directory.
             img.save(img_path)
             current_sample += 1
 
+def calculate_max_height(in_dir):
+    """
+    Goes through all lines and finds maximal rendered image height.
+
+    Args:
+        in_dir - Path to input directory containing textual files.
+    """
+    height = 0
+    files = os.listdir(in_dir)
+    # Go over all files.
+    for file in files:
+        file_path = os.path.join(in_dir, file)
+        # Determine language based on file name.
+        language = os.path.splitext(file)[0]
+        # Take appropriate font for language.
+        font_name = font_by_language[language]
+        # Load all lines from the file.
+        in_file = open(file_path, "r", encoding="utf-8")
+        text_lines = in_file.read().split("\n")
+        in_file.close()
+        # Go over lines.
+        for text_line in text_lines:
+            if text_line == "":
+                continue
+            img = Image.new('RGB', (10, 10), color = 'white')
+            img_draw = ImageDraw.Draw(img)
+            fnt = ImageFont.truetype(font_name, 40)
+            text_size = img_draw.textsize(text_line, font=fnt)
+            if text_size[1] > height:
+                height = text_size[1]
+
+    return height
+
 if __name__ == "__main__":
-    # Parse arguments.
+    # Parse arguments. 
     parser = argparse.ArgumentParser()
     parser.add_argument('-in_dir', help='Path to input dir where text files are be stored.', required=True)
     parser.add_argument('-out_dir', help='Path to output dir where images will be stored.', required=True)
     args = vars(parser.parse_args())
+    # Calculate maximal image height.
+    height = calculate_max_height(args["in_dir"])
     # Create images and place them in output directory.
-    create_images(args["in_dir"], args["out_dir"])
+    create_images(args["in_dir"], args["out_dir"], height)
