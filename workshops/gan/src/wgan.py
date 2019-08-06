e2902426f001ec0413a43b0b9c5ac13c0956e1fe
Abi Gicic <gicic.abdulah@gmail.com>
2019-08-06 08:11:37 +0200

    Alt+shift+f

--- workshops/gan/src/wgan.py
+++ workshops/gan/src/wgan.py
@@ -35,7 +35,8 @@ class WGAN:
         self.z = tf.placeholder(tf.float32, [None, self.z_size], name="Z")
         # image shape is [batch_size, height, width, channels]
         self.real_image = tf.placeholder(tf.float32,
-                                         [None, self.dataset.img_size, self.dataset.img_size, self.dataset.channels],
+                                         [None, self.dataset.img_size,
+                                             self.dataset.img_size, self.dataset.channels],
                                          name="Real_image")
         """
         ##################################################################
@@ -69,8 +70,10 @@ class WGAN:
 
         # Critic regularization, satisfying the Lipschitz constraint with gradient penalty
         with tf.name_scope("Gradient_penalty"):
-            self.eta = tf.placeholder(tf.float32, shape=[None, 1, 1, 1], name="Eta")
-            interp = self.eta * self.real_image + (1 - self.eta) * self.fake_image
+            self.eta = tf.placeholder(
+                tf.float32, shape=[None, 1, 1, 1], name="Eta")
+            interp = self.eta * self.real_image + \
+                (1 - self.eta) * self.fake_image
             c_interp = self.critic(interp, reuse=True)
 
             # taking the zeroth and only element because tf.gradients returns a list
@@ -96,11 +99,15 @@ class WGAN:
         ##################################################################
         """
 
-        c_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "Critic")  # weights for the critic
-        self.c_optimizer = self.optimizer.minimize(self.c_cost, var_list=c_variables, name="Critic_optimizer")
+        c_variables = tf.get_collection(
+            tf.GraphKeys.TRAINABLE_VARIABLES, "Critic")  # weights for the critic
+        self.c_optimizer = self.optimizer.minimize(
+            self.c_cost, var_list=c_variables, name="Critic_optimizer")
 
-        g_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "Generator")  # weights for the generator	
-        self.g_optimizer = self.optimizer.minimize(self.g_cost, var_list=g_variables, name="Generator_optimizer")
+        g_variables = tf.get_collection(
+            tf.GraphKeys.TRAINABLE_VARIABLES, "Generator")  # weights for the generator
+        self.g_optimizer = self.optimizer.minimize(
+            self.g_cost, var_list=g_variables, name="Generator_optimizer")
 
         """   
         ##################################################################
@@ -111,8 +118,10 @@ class WGAN:
         """
 
         # Defining summaries for tensorflow until the end of the method
-        tf.summary.image("Generated image", self.fake_image, max_outputs=WGAN.max_summary_images)
-        tf.summary.image("Real image", self.real_image, max_outputs=WGAN.max_summary_images)
+        tf.summary.image("Generated image", self.fake_image,
+                         max_outputs=WGAN.max_summary_images)
+        tf.summary.image("Real image", self.real_image,
+                         max_outputs=WGAN.max_summary_images)
         tf.summary.scalar("Critic cost", self.c_cost)
         tf.summary.scalar("Generator cost", self.g_cost)
 
@@ -124,7 +133,8 @@ class WGAN:
             else:
                 grad_values = gradient
             tf.summary.histogram(variable.name, variable)
-            tf.summary.histogram(os.path.join(variable.name, "gradients"), grad_values)
+            tf.summary.histogram(os.path.join(
+                variable.name, "gradients"), grad_values)
 
         self.merged = tf.summary.merge_all()
 
@@ -179,13 +189,15 @@ class WGAN:
             c_times = 5
 
         for _ in range(c_times):
-            eta = np.random.rand(batch_size, 1, 1, 1)  # sampling from uniform distribution	
-            data_batch = self.dataset.next_batch_real(batch_size)	
-            z = self.dataset.next_batch_fake(batch_size, self.z_size)	
+            # sampling from uniform distribution
+            eta = np.random.rand(batch_size, 1, 1, 1)
+            data_batch = self.dataset.next_batch_real(batch_size)
+            z = self.dataset.next_batch_fake(batch_size, self.z_size)
 
-            sess.run(self.c_optimizer, feed_dict={self.real_image: data_batch, self.z: z, self.eta: eta})	
+            sess.run(self.c_optimizer, feed_dict={
+                     self.real_image: data_batch, self.z: z, self.eta: eta})
 
-        z = self.dataset.next_batch_fake(batch_size, self.z_size)	
+        z = self.dataset.next_batch_fake(batch_size, self.z_size)
         sess.run(self.g_optimizer, feed_dict={self.z: z})
 
         """
@@ -209,6 +221,8 @@ class WGAN:
         z = self.dataset.next_batch_fake(WGAN.max_summary_images, self.z_size)
         eta = np.random.rand(WGAN.max_summary_images, 1, 1, 1)
 
-        summary = sess.run(self.merged, feed_dict={self.real_image: data_batch, self.z: z, self.eta: eta})
+        summary = sess.run(self.merged, feed_dict={
+                           self.real_image: data_batch, self.z: z, self.eta: eta})
         writer.add_summary(summary, step)
-        print("\rSummary generated. Step", step, " Time == %.2fs" % timer.time())
+        print("\rSummary generated. Step", step,
+              " Time == %.2fs" % timer.time())
