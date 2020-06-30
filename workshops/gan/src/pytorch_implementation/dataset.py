6096e6d3ed663642046b31815b3f68232ecf2d8e
Abi Gicic <abgicic@microsoft.com>
2020-06-30 19:23:25 +0200

    Convert the ws to pytorch - first commit

---
+++ workshops/gan/src/pytorch_implementation/dataset.py
@@ -0,0 +1,32 @@
+import os
+import glob
+import torch
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+
+class FacesDataSet(Dataset):
+    def __init__(self, img_size, crop_size=128):
+        assert img_size <= crop_size <= 250
+        self.img_size = img_size
+        self.crop_size = crop_size
+        project_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__))) # should be PSIML/workshops/gan
+        project_path = os.path.abspath(project_path)
+        self.data_root = os.path.join(project_path, "data", "lfw-deepfunneled")
+        self.image_paths = glob.glob(os.path.join(self.data_root, "**", "*.jpg"))
+        self.transforms = transforms.Compose(
+            [
+                transforms.CenterCrop((self.crop_size, self.crop_size)),
+                transforms.Resize(self.img_size),
+                transforms.ToTensor() # this puts the image in the CxHxW format and normalizes it to [0,1). See if we want this!
+            ]
+        )
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, index):
+        image_path =  self.image_paths[index]
+        image = Image.open(image_path)
+        return self.transforms(image)
---
+++ workshops/gan/src/pytorch_implementation/training.py
@@ -0,0 +1,76 @@
+import torch
+from torch.utils.tensorboard import SummaryWriter  # TODO: use it
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from critics import FCCritic
+from generators import FCGenerator
+from dataset import FacesDataSet
+
+IMG_SIZE = 128
+CHANNELS = 3
+Z_SIZE = 100
+
+CUDA = True if torch.cuda.is_available() else False
+LR = 1e-4
+EPOCHS = 1
+BATCH_SIZE = 8
+NUM_WORKERS = 0  # set to 0 when debugging
+CLIP_VALUE = 1e-2
+
+def train():
+    critic = FCCritic(IMG_SIZE, CHANNELS)
+    generator = FCGenerator(IMG_SIZE, CHANNELS, Z_SIZE)
+    if CUDA:
+        critic.cuda()
+        generator.cuda()
+
+    data_loader = DataLoader(FacesDataSet(IMG_SIZE),
+                            batch_size=BATCH_SIZE,
+                            shuffle=True,
+                            pin_memory=True,
+                            num_workers=NUM_WORKERS)
+
+    optimizer_C = torch.optim.RMSprop(critic.parameters(), lr=LR)
+    optimizer_G = torch.optim.RMSprop(critic.parameters(), lr=LR)
+
+    for epoch in range(EPOCHS):
+        if epoch < 25:
+            c_times = 100
+        else:
+            c_times = 5
+
+        for i, real_img_batch in tqdm(enumerate(data_loader)):
+            z_batch = torch.rand(BATCH_SIZE, Z_SIZE)
+            if CUDA:
+                real_img_batch = real_img_batch.cuda()
+                z_batch = z_batch.cuda()
+
+            optimizer_C.zero_grad()
+            fake_img_batch = generator(z_batch).detach()
+            # Adversarial loss
+            loss_c = -torch.mean(critic(real_img_batch)) + torch.mean(critic(fake_img_batch))
+
+            # Calculate the gradients with respect to the input
+            loss_c.backward()
+            # Apply backward prop
+            optimizer_C.step()
+
+            # Clip the weights of the discriminator to satisfy the Lipschitz constraint
+            for p in critic.parameters():
+                p.data.clamp_(-CLIP_VALUE, CLIP_VALUE)
+
+            # Train the generator only after the discriminator has been trained c_times
+            if i % c_times == 0:
+                optimizer_G.zero_grad()
+
+                # Generate a batch of images
+                gen_imgs = generator(z_batch)
+                # Adversarial loss
+                loss_G = -torch.mean(critic(gen_imgs))
+
+                loss_G.backward()
+                optimizer_G.step()
+
+if __name__ == "__main__":
+    train()
