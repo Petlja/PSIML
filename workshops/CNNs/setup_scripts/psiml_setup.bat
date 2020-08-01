9ac35ac356fd7ba2013c6e52280db321d6c4d605
Nikola Dimitrijevic <zacementirano@gmail.com>
2020-08-01 10:40:56 +0200

    open cnn_ws on notebook boot

--- workshops/CNNs/setup_scripts/psiml_setup.bat
+++ workshops/CNNs/setup_scripts/psiml_setup.bat
@@ -4,6 +4,6 @@ cd .\PSIML\workshops\CNNs & ^
 conda env create -f environment_gpu.yml & ^
 conda activate psiml_gpu & ^
 echo "Setup success! Starting jupyter notebook" & ^
-jupyter-notebook ^ &
+jupyter-notebook CNN_WS.ipynb ^ &
 cmd /k 
 
